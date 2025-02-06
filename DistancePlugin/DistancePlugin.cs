1631f831f2b0e7bdec2edf9cd55007f35dfa256d
Trevor Jones <jones1876@gmail.com>
2025-02-06 15:02:32 -0500

    Distance Plugin Added

---
+++ DistancePlugin/DistancePlugin.cs
@@ -0,0 +1,172 @@
+﻿using SharedLib;
+using SharedLib.TelemetryHelper;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Threading;
+
+using YawGLAPI;
+
+namespace YawVR_Game_Engine.Plugin
+{
+    [Export(typeof(Game))]
+    [ExportMetadata("Name", "Distance")]
+    [ExportMetadata("Version", "1.0")]
+
+    public class DistancePlugin : Game
+    {
+
+
+
+        private UdpTelemetry<DistanceTelemetryData> telem;
+        private Thread readThread;
+        private IMainFormDispatcher dispatcher;
+        private IProfileManager controller;
+        private volatile bool running = false;
+        public int STEAM_ID => 233610;
+        public string PROCESS_NAME => "Distance";
+        public string AUTHOR => "Trevor Jones";
+
+        public bool PATCH_AVAILABLE => false;
+
+        public string Description => ResourceHelper.GetString("description.html");
+        public Stream Logo => ResourceHelper.GetStream("logo.png");
+        public Stream SmallLogo => ResourceHelper.GetStream("recent.png");
+        public Stream Background => ResourceHelper.GetStream("wide.png");
+        private string defProfile => ResourceHelper.GetString("Default.yawglprofile");
+
+
+
+
+        public void Exit()
+        {
+            running = false;
+            telem?.Dispose();
+        }
+        public void PatchGame()
+        {
+            return;
+        }
+
+
+        public string[] GetInputData() => InputHelper.GetInputs<DistanceTelemetryData>(default).Select(_ => _.key).ToArray();
+
+        public LedEffect DefaultLED() => new LedEffect(EFFECT_TYPE.KNIGHT_RIDER, 0, new[] { YawColor.WHITE }, 0); 
+        public List<Profile_Component> DefaultProfile() => dispatcher.JsonToComponents(defProfile);
+            
+        public void SetReferences(IProfileManager controller, IMainFormDispatcher dispatcher)
+        {
+            this.dispatcher = dispatcher;
+            this.controller = controller;
+        }
+
+        public void Init()
+        {
+            running = true;
+            readThread = new Thread(new ThreadStart(ReadThread));
+            readThread.Start();
+        }
+        private void ReadThread()
+        {
+            try
+            {
+                telem = new UdpTelemetry<DistanceTelemetryData>(new UdpTelemetryConfig
+                {
+                    ReceiveAddress = new IPEndPoint(IPAddress.Any, 12345)
+                });
+            
+            }
+            catch(Exception x)
+            {
+                dispatcher.ShowNotification(NotificationType.ERROR, x.Message);
+                Exit();
+            }
+
+            while (running)
+            {
+                try
+                {
+                    var data = telem.Receive();
+
+                    if (data.IsCarIsActive)
+                    {
+                        if (!isRestting)
+                        {
+                            foreach (var (i, (key, value)) in InputHelper.GetInputs(data).WithIndex())
+                            {
+                                if (key == "Pitch" || key == "Roll")
+                                {
+                                    var v = 0f;
+                                    if (Math.Abs(value) <= 90)
+                                    {
+                                        v = value;
+                                    }
+                                    else
+                                    {
+                                        v = (180 - Math.Abs(value)) * (value < 0 ? -1 : 1);
+                                    }
+                                    var max = 90;
+                                    var nv = (float)EnsureMapRange(v, -90, 90, -max, max);
+                                    controller.SetInput(i, nv);
+                                    continue;
+                                }
+
+                                controller.SetInput(i, value);
+                            }
+                        }
+                        else
+                        {
+                            isRestting = false;
+                            Thread.Sleep(1000);
+                        }
+                    }
+                    else if (!isRestting)
+                    {
+                        isRestting = true;
+                    }
+                }
+                catch(SocketException sex) { }
+            }
+            
+        }
+
+        bool isRestting = false;
+
+
+
+        public Dictionary<string, ParameterInfo[]> GetFeatures() => null;
+
+        public static double MapRange(double x, double xMin, double xMax, double yMin, double yMax)
+        {
+            return yMin + (yMax - yMin) * (x - xMin) / (xMax - xMin);
+        }
+
+        public static double EnsureMapRange(double x, double xMin, double xMax, double yMin, double yMax)
+        {
+            return Math.Max(Math.Min(MapRange(x, xMin, xMax, yMin, yMax), Math.Max(yMin, yMax)), Math.Min(yMin, yMax));
+        }
+    }
+
+    static class Extensions
+    {
+        
+
+        public static IEnumerable<(int index, T value)> WithIndex<T>(this IEnumerable<T> source)
+        {
+            int index = 0;
+            foreach (var item in source)
+            {
+                yield return (index, item);
+                index++;
+            }
+        }
+    }
+
+}
---
+++ DistancePlugin/SharedLib/TelemetryHelper/TelemetryBase.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SharedLib.TelemetryHelper
+{
+    internal interface ITelemetry<TData, TConfig> 
+        where TData : struct 
+        where TConfig : class, new()
+    {
+        event TelemetryBase<TData, TConfig>.LogEventHandler OnLog;
+
+        int Send(TData data);
+
+        TData Receive();
+
+
+        Task<TData> ReceiveAsync(CancellationToken cancellationToken = default);
+
+    }
+
+    internal abstract class TelemetryBase<TData, TConfig> : ITelemetry<TData, TConfig>, IDisposable
+        where TData : struct
+        where TConfig : class, new()
+    {
+        public TConfig Config { get; private set; }
+
+        public delegate void LogEventHandler(object sender, string message);
+        public event LogEventHandler OnLog;
+
+        protected abstract void Configure(TConfig config);
+        public abstract int Send(TData message);
+        public abstract TData Receive();
+
+        protected TelemetryBase(TConfig config)
+        {
+            Config = config ?? new TConfig();
+            Configure(Config);
+        }
+
+        protected void Log(string message)
+        {
+            OnLog?.Invoke(this, $"[{this.GetType().Name}] " + message);
+        }
+
+        virtual protected byte[] ToBytes<T>(T data) where T : struct
+        {
+            int size = Marshal.SizeOf(data);
+            byte[] arr = new byte[size];
+            using (SafeBuffer buffer = new SafeBuffer(size))
+            {
+                Marshal.StructureToPtr(data, buffer.DangerousGetHandle(), true);
+                Marshal.Copy(buffer.DangerousGetHandle(), arr, 0, size);
+            }
+            return arr;
+        }
+
+        virtual protected T FromBytes<T>(byte[] data) where T : struct
+        {
+            T result = default(T);
+            using (SafeBuffer buffer = new SafeBuffer(data.Length))
+            {
+                Marshal.Copy(data, 0, buffer.DangerousGetHandle(), data.Length);
+                result = (T)Marshal.PtrToStructure(buffer.DangerousGetHandle(), typeof(T));
+            }
+            return result;
+        }
+
+        public abstract void Dispose();
+
+        public abstract Task<TData> ReceiveAsync(CancellationToken cancellationToken = default);
+
+    }
+
+    internal class SafeBuffer : SafeHandle
+    {
+        public SafeBuffer(int size) : base(IntPtr.Zero, true)
+        {
+            SetHandle(Marshal.AllocHGlobal(size));
+        }
+
+        public override bool IsInvalid => handle == IntPtr.Zero;
+
+        protected override bool ReleaseHandle()
+        {
+            Marshal.FreeHGlobal(handle);
+            return true;
+        }
+    }
+}

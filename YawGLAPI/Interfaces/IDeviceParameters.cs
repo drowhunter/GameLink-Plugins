0246ff0b2021097db07386ff6f567ff1f861e488
Zettisch Dániel <zettisch.daniel@yawvr.com>
2025-02-12 13:10:21 +0100

    Expose DeviceParameters through plugin API

---
+++ YawGLAPI/Interfaces/IDeviceParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace YawGLAPI
+{
+    public interface IDeviceParameters
+    {
+
+        public byte Power { get; }
+        public byte MaxRoll { get; }
+        public byte MaxPitchFW { get; }
+        public byte MaxPitchBW { get; }
+        public byte RollLimit { get; }
+        public byte PitchLimitF { get; }
+        public byte PitchLimitB { get; }
+        public uint YawLimit { get; }
+        public float YawReturn { get; }
+        public bool HasYawLimit { get; }
+        public bool HasVibration {  get; }
+        public byte VibrationLimit {  get; }
+    }
+}

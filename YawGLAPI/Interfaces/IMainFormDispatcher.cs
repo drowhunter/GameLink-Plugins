f388301108c01a3540ca38940023f675537cfd25
Zettisch Dániel <zettisch.daniel@yawvr.com>
2025-02-12 13:10:21 +0100

    Expose DeviceParameters through plugin API

--- YawGLAPI/Interfaces/IMainFormDispatcher.cs
+++ YawGLAPI/Interfaces/IMainFormDispatcher.cs
@@ -6,7 +6,7 @@ namespace YawGLAPI
     public interface IMainFormDispatcher
     {
 
-
+        public IDeviceParameters GetDeviceParameters();
         public string GetLanguage();
         public void ShowNotification(NotificationType type, string text);
         public void DialogShow(string _string, DIALOG_TYPE type, Action<bool> _yes = null, Action<bool> _no = null, bool showChk = false, bool chkDefault = false,string chkText = "");

46e7c121a81e56f1aa6971288e77766436d52831
Trevor Jones <jones1876@gmail.com>
2025-02-21 00:44:45 -0500

    [nsgp] added New Star GP plugin version 1 of nsgp plugin add experimental patching prettify nsgp profile

---
+++ SharedLib/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLib
+{
+    internal static class Extensions
+    {
+        //public static IEnumerable<(int index, T value)> WithIndex<T>(this IEnumerable<T> source)
+        //{
+        //    int index = 0;
+        //    foreach (var item in source)
+        //    {
+        //        yield return (index, item);
+        //        index++;
+        //    }
+        //}
+
+        public static IEnumerable<(int index, string key, float value)> WithIndex(this IEnumerable<(string key, float value)> source)
+        {
+            int index = 0;
+            foreach (var (k,v) in source)
+            {
+                yield return (index, k, v);
+                index++;
+            }
+        }
+    }
+}
---
+++ SharedLib/Validatior.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLib
+{
+    internal class Validator
+    {
+        public const string IP_ADDRESS = @"^\d{1,3}(\.\d{1,3}){3}$";
+        public const string PORT = @"^\d{1,5}$";
+    }
+}

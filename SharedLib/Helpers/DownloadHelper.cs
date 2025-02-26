ff1fa290f3a0e1d992e812bb5035d680b4dd385b
Trevor Jones <jones1876@gmail.com>
2025-02-26 00:45:34 -0500

    add new helpers

---
+++ SharedLib/Helpers/DownloadHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+using YawGLAPI;
+
+namespace SharedLib
+{
+    internal class DownloadHelper
+    {
+        public static async Task<string?> DownloadFileAsync(string url, CancellationToken cancellationToken = default)
+        {
+
+            using var httpClient = new System.Net.Http.HttpClient();
+            string filename = Path.GetTempFileName();
+            Console.WriteLine($"Downloading {url} to {filename}");
+            try
+            {
+                var response = await httpClient.GetAsync(url, cancellationToken);
+                if (response.IsSuccessStatusCode)
+                {
+                    var fileBytes = await response.Content.ReadAsByteArrayAsync(cancellationToken);
+
+                    var fn = response.Content.Headers?.ContentDisposition?.FileName;
+                    Console.WriteLine($"Downloaded {fn} {fileBytes.Length} bytes");
+
+                    await File.WriteAllBytesAsync(filename, fileBytes, cancellationToken);
+
+                    return filename;
+                }
+                else
+                {
+                    throw new Exception("Error downloading file: " + response.ReasonPhrase);
+
+                }
+            }
+            catch (Exception e)
+            {
+                
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SharedLib/Helpers/IniHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SharedLib
+{
+    internal class IniHelper
+    {
+        public static async Task<Dictionary<string, string>> LoadFileAsync(string path, CancellationToken cancellationToken = default)
+            => Parse(await File.ReadAllTextAsync(path, cancellationToken));
+
+        public static Dictionary<string, string> Parse(string ini)
+        {
+            var dict = new Dictionary<string, string>();
+            foreach (var line in ini.Split('\n'))
+            {
+                var parts = line.Split('=');
+                if (parts.Length == 2)
+                    dict[parts[0].Trim()] = parts[1].Trim();
+            }
+            return dict;
+        }
+
+    }
+}
